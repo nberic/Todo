9f3463c0641dc8075868ed26ae62c5c7392349ca
nberic <ct.nberic@gmail.com>
2019-12-21 18:23:26 +0100

    Create Header for the page. Create component for adding new todoItems.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { AddTodoItemComponent } from './components/add-todo-item/add-todo-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoListComponent,
-    TodoItemComponent
+    TodoItemComponent,
+    HeaderComponent,
+    AddTodoItemComponent
   ],
   imports: [
     BrowserModule,
